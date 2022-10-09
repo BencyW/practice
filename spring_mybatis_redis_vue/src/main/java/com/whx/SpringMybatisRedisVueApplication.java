3a843d5a6edb8bf81944d6d99b945cb3cc7210d0
BencyW <1054430014@qq.com>
2022-10-09 14:07:13 +0800

    spring_mybatis_redis_vue

---
+++ spring_mybatis_redis_vue/src/main/java/com/whx/SpringMybatisRedisVueApplication.java
@@ -0,0 +1,13 @@
+package com.whx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMybatisRedisVueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMybatisRedisVueApplication.class, args);
+    }
+
+}
